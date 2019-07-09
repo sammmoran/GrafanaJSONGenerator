443f29555a5ee8c055a418ec9797b589d75c9307
Sam Moran <srmoran93@gmail.com>
2019-07-09 15:29:41 -0400

    2nd Commit: => DS JSON.txt file was created as a template for generator program => id_list.txt file will contain the IDs needed to construct DashBoards

--- generator.py
+++ generator.py
@@ -1,14 +1,33 @@
 
 import argparse
+from string import Template
 
 
 def main(files):
 
+    #Construct JSON Data Source and Dash Board feed files for Grafana
+    ds = open("DS_feed.JSON","w+")
+    db = open("DB_feed.JSON","w+")
+
+    #Open the id file
+    id_list = open("id_list.txt","r")
+
+    for id in id_list:
+        print(id)
+
+
+    id_list.close()
+
+    
+    #We are done with creating the JSON files
+    ds.close()
+    db.close()
     
 
 
 
 
+#Program starts here
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
@@ -17,4 +36,6 @@ if __name__=="__main__":
     #Parse the files separately
     args_namespace=parser.parse_args()
     args=vars(args_namespace)['file']
+
+    #Send files to main function
     main(args)
